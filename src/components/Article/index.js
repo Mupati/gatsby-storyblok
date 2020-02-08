207a9c8fe26c247401d020814586466c0c434362
Mupati <scientificgh@gmail.com>
2020-02-08 10:34:48 +0000

    add disqus to handle comments

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,21 +1,30 @@
 import React from "react"
 import styled from "styled-components"
 import RichText from "../../utils/richTextRenderer"
+import { DiscussionEmbed } from "disqus-react"
 
 const Article = props => {
+  const disqusShortname = "devcodes"
+  const disqusConfig = {
+    url: `https://www.devcodes.co/${props.blok.meta[0].link.cached_url}`,
+    identifier: props.blok._uid,
+    title: props.blok.meta[0].title,
+  }
   return (
     <Wrapper>
-      <RichText data={props.blok.body} />
+      <ArticleWrapper>
+        <RichText data={props.blok.body} />
+      </ArticleWrapper>
+      <StyledDisqus>
+        <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+      </StyledDisqus>
     </Wrapper>
   )
 }
 
 export default Article
-
-const Wrapper = styled.article`
+const Wrapper = styled.section`
   font: 400 18px/33px "Montserrat";
-  display: flex;
-  justify-content: center;
   margin: 2em 15% 5em;
   @media (max-width: 768px) {
     margin-left: 5%;
@@ -30,4 +39,12 @@ const Wrapper = styled.article`
     margin-left: 2.5%;
     margin-right: 2.5%;
   }
+`
+
+const ArticleWrapper = styled.article`
+  display: flex;
+  justify-content: center;
+`
+const StyledDisqus = styled.div`
+  margin-top: 2em;
 `
