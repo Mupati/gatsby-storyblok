37daa3ffc7042fa7fa557df7c2d215719f05d9e2
Mupati <scientificgh@gmail.com>
2020-03-03 14:10:55 +0000

    Update Code styles

--- src/utils/richTextRenderer.js
+++ src/utils/richTextRenderer.js
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from "styled-components"
 const StoryblokClient = require("storyblok-js-client")
 
 let Storyblok = new StoryblokClient({
@@ -7,7 +8,7 @@ let Storyblok = new StoryblokClient({
 
 const RichTextContent = ({ data }) => {
   return (
-    <div
+    <Wrapper
       dangerouslySetInnerHTML={{
         __html: Storyblok.richTextResolver.render(data),
       }}
@@ -15,3 +16,21 @@ const RichTextContent = ({ data }) => {
   )
 }
 export default RichTextContent
+
+const Wrapper = styled.div`
+  font: 400 18px/33px "Montserrat";
+  margin: 2em 15% 5em;
+  @media (max-width: 768px) {
+    margin-left: 5%;
+    margin-right: 5%;
+  }
+  @media (max-width: 768px) {
+    margin-left: 5%;
+    margin-right: 5%;
+  }
+  @media (max-width: 480px) {
+    font-size: 16px;
+    margin-left: 2.5%;
+    margin-right: 2.5%;
+  }
+`
